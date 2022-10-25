8ba1963d084a4c7d5a49c2a6cdb413ed4a5e99e9
dextr7 <action@h7ml.cn>
2022-10-25 17:58:35 +0800

    feat(custom): cz-config

--- packages/cz-config/lib/index.js
+++ packages/cz-config/lib/index.js
@@ -1,32 +1,42 @@
 module.exports = {
   // 可选类型
   types: [
-    { value: 'feat', name: 'feat:  新功能' },
-    { value: 'fix', name: 'fix:  修复' },
-    { value: 'docs', name: 'docs:  文档变更' },
-    { value: 'style', name: 'style:  代码格式(不影响代码运行的变动)' },
+    { value: 'feat', name: 'feat:     新增功能' },
+    { value: 'fix', name: 'fix:      修复bug' },
+    { value: 'docs', name: 'docs:     文档变更' },
+    {
+      value: 'style',
+      name: 'style:    代码格式（不影响功能，例如空格、分号等格式修正）',
+    },
     {
       value: 'refactor',
-      name: 'refactor:  重构(既不是增加feature，也不是修复bug)',
+      name: 'refactor: 代码重构（不包括 bug 修复、功能新增）',
     },
-    { value: 'perf', name: 'perf:  性能优化' },
-    { value: 'test', name: 'test:  增加测试' },
-    { value: 'chore', name: 'chore:  构建过程或辅助工具的变动' },
-    { value: 'revert', name: 'revert:  回退' },
-    { value: 'merge', name: 'merge:  合并' },
-    { value: 'build', name: 'build:  打包' },
+    { value: 'perf', name: 'perf:     性能优化' },
+    { value: 'test', name: 'test:     添加、修改测试用例' },
+    {
+      value: 'build',
+      name: 'build:    构建流程、外部依赖变更（如升级 npm 包、修改 脚手架 配置等）',
+    },
+    { value: 'ci', name: 'ci:       修改 CI 配置、脚本' },
+    {
+      value: 'chore',
+      name: 'chore:    对构建过程或辅助工具和库的更改（不影响源文件、测试用例）',
+    },
+    { value: 'revert', name: 'revert:   回滚 commit' },
   ],
   // 消息步骤
   messages: {
-    type: '请选择提交类型:',
-    customScope: '请输入修改范围(可选):',
-    subject: '请简要描述提交(必填):',
-    body: '请输入详细描述(可选):',
-    footer: '请输入要关闭的issue(可选):',
-    confirmCommit: '确认使用以上信息提交？(y/n/e/h)',
+    type: '确保本次提交遵循 Angular 规范！\n选择你要提交的类型：',
+    scope: '\n选择一个 scope（可选）：',
+    customScope: '请输入自定义的 scope：',
+    subject: '填写简短精炼的变更描述：\n',
+    body: '填写更加详细的变更描述（可选）。使用 "|" 换行：\n',
+    breaking: '列举非兼容性重大的变更（可选）：\n',
+    footer: '列举出所有变更的 ISSUES CLOSED（可选）。 例如: #31, #34：\n',
+    confirmCommit: '确认提交？',
   },
-  // 跳过问题
-  skipQuestions: ['body', 'footer'],
-  // subject文字长度默认是72
-  subjectLimit: 72,
+  allowBreakingChanges: ['feat', 'fix'],
+  subjectLimit: 100,
+  breaklineChar: '|',
 }
